95a31aef507b8b18839b03208a735021c7be50a7
LAZcoder123 <louisz1@gmx.de>
2025-07-28 22:36:20 +0200

    Fix: Remove duplicate declaration of 'app' in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const path = require("path");
 const webpush = require("web-push");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors({
+  origin: "https://klassisches-reaktionsspiel.netlify.app"
+}));
+
+
+
 app.use(express.static(path.join(__dirname, "frontend")));
 app.use(express.json());
 
